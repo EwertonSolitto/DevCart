fcdbe084143b4e431a0f3d2144e943b2475ca42c
Ewerton <ewerton2004@yahoo.com.br>
2025-06-07 22:52:37 -0300

    feat: add icons to tabs and create badge to favorite and cart tabs

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 import HomeScreen from '../screens/HomeScreen';
 import CartScreen from '../screens/CartScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
 import ProductScreen from '../screens/ProductScreen';
+import { useFavoriteStore } from '../store/favoriteStore';
+import { useCartStore } from '../store/cartStore';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -30,12 +33,36 @@ function FavoriteStack() {
 }
 
 export default function AppNavigator() {
+  const favoriteCount = useFavoriteStore((state) => state.favorites.length)
+  const cartCount = useCartStore((state) => (
+    state.cart.reduce((total, item) => total + item.quantity, 0)
+  ))
+
   return (
     <NavigationContainer>
-      <Tab.Navigator screenOptions={{ headerShown: false }}>
+      <Tab.Navigator screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ color, size }) => {
+          if (route.name === 'Home') return <FontAwesome name='home' size={size} color={color} />
+          if (route.name === 'Favorites') return <FontAwesome name='heart' size={size} color={color} />
+          if (route.name === 'Cart') return <FontAwesome name='shopping-cart' size={size} color={color} />
+        }
+      })}>
         <Tab.Screen name="Home" component={HomeStack} />
-        <Tab.Screen name="Favorites" component={FavoriteStack} />
-        <Tab.Screen name="Cart" component={CartScreen} />
+        <Tab.Screen 
+          name="Favorites" 
+          component={FavoriteStack} 
+          options={{
+            tabBarBadge: favoriteCount > 0 ? favoriteCount : undefined
+          }}
+        />
+        <Tab.Screen
+          name="Cart" 
+          component={CartScreen} 
+          options={{
+            tabBarBadge: cartCount > 0 ? cartCount : undefined
+          }}  
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
