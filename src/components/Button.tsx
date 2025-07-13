56388c204eadfda445d44ad87475458eb1423760
Ewerton <ewerton2004@yahoo.com.br>
2025-07-13 00:34:00 -0300

    refactor: add disabled to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { colors } from '../theme/themes';
 
-export function LinkButton({ text, func }: { text: string, func: () => void}) {
+export function LinkButton({ text, func, disabled }: { text: string, func: () => void, disabled?: boolean}) {
   return (
-    <TouchableOpacity style={styles.container} onPress={func}>
+    <TouchableOpacity style={styles.container} onPress={func} disabled={disabled}>
       <Text style={styles.text}>{text}</Text>
     </TouchableOpacity>
   );
